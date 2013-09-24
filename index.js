1672d5339cef300a50a776b4323f63c3827dd299
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-09-24 16:42:13 +0600

    Refactored handle of out and dev options

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ var Temp = require('temporary/lib/dir');
 var builder = require('./builder.js');
 
 module.exports = function(opts) {
+  if (!opts.out) opts.out = new Temp().path;
+
   var router = express();
-
-  opts.dev = 'dev' in opts ? opts.dev : true;
-  opts.out = new Temp().path;
-
-  router.use(express.static(opts.out));
-
   var rebuild = builder(opts);
 
+  router.use(express.static(opts.out));
   router.get('/build.js', rebuild, function(req, res) {
     res.type('js');
     res.send(res.locals.js);
